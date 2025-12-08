564f9b1773e37d3452418483f36ff28100f4427b
Paul Angeles <paul.angeles.chaquire@gmail.com>
2025-12-08 09:24:58 +0100

    com errors

--- join/src/app/app.ts
+++ join/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './shared/header/header';
-import { Sidebar } from './shared/sidebar/sidebar';
+// import { Header } from './shared/header/header';
+// import { Sidebar } from './shared/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Sidebar],
+  imports: [RouterOutlet],
+  // imports: [RouterOutlet, Header, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
