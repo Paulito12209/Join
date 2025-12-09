b1072f8c2c08182be064dd34cf0b031ef5e25a39
Paul Angeles <paul.angeles.chaquire@gmail.com>
2025-12-09 23:13:58 +0100

    add scss styles global & routerLink logic

--- join/src/app/app.routes.ts
+++ join/src/app/app.routes.ts
@@ -1,10 +1,20 @@
 import { Routes } from '@angular/router';
 import { Contacts } from './pages/contacts/contacts';
 import { MainLayout } from './core/layouts/main-layout/main-layout';
+import { Summary } from './pages/summary/summary';
+import { Board } from './pages/board/board';
+import { AddTask } from './pages/add-task/add-task';
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { LegalNotes } from './pages/legal-notes/legal-notes';
 
 export const routes: Routes = [{
     path: "", component: MainLayout, children: [
         { path: "contacts", component: Contacts },
+        { path: "summary", component: Summary },
+        { path: "board", component: Board },
+        { path: "add-task", component: AddTask },
+        { path: "privacy-policy", component: PrivacyPolicy },
+        { path: "legal-notice", component: LegalNotes },
         { path: "", redirectTo: "contacts", pathMatch: "full" }
     ]
 }];
--- join/src/app/core/layouts/main-layout/main-layout.ts
+++ join/src/app/core/layouts/main-layout/main-layout.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { Sidebar } from '../../../shared/components/sidebar/sidebar';
 import { Header } from '../../../shared/components/header/header';
-import { Contacts } from '../../../pages/contacts/contacts';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [Sidebar, Header, Contacts],
+  imports: [Sidebar, Header, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
