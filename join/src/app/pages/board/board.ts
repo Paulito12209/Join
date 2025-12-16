d33a2691b5ec869e388b2c1307434298dab43e58
Paul Angeles <paul.angeles.chaquire@gmail.com>
2025-12-16 15:47:11 +0100

    add board page

--- join/src/app/pages/board/board.ts
+++ join/src/app/pages/board/board.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList,
+} from "@angular/cdk/drag-drop";
 
 @Component({
-  selector: 'app-board',
-  imports: [],
-  templateUrl: './board.html',
-  styleUrl: './board.scss',
+  selector: "app-board",
+  imports: [CdkDropList, CdkDrag],
+  templateUrl: "./board.html",
+  styleUrl: "./board.scss",
 })
 export class Board {
+  todo = ["Wenn eine Spalte ohne Tasks steht hier eine Info, dass keine Tasks in dem jeweiligen Status sich befinden", "Jeder Task zeigt Kategorie, Titel, eine Vorschau der Beschreibung, alle zugewiesenen Benutzer mit Initialen und die Priorität des Tasks.", "Ich kann die vollständige Beschreibung und alle Infos zu einem Tasks anzeigen, wenn ich auf einen Task klicke."];
+  progress = [
+    "Das Board hat ein Layout mit vier Spalten: ToDo, In Progress, Awaiting Feedback und Done.",
+    "Es gibt ein '+'-Icon in jeder Spalte außer der Kategorie “Done”, dass das Hinzufügen eines neuen Tasks ermöglicht.",
+  ];
+  feedback = [
+    "Platzhalter Feedback",
+  ];
+  done = ["Platzhalter Done"];
 
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 }
