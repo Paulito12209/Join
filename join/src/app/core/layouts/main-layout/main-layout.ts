315d32f4c84f9eb5ca04b7dd30fb8b78eb3882d6
Dani S. <daniela.schomaker@posteo.de>
2026-01-08 13:29:00 +0100

    fix(navigation): initialize navigation tracking in main layout

--- join/src/app/core/layouts/main-layout/main-layout.ts
+++ join/src/app/core/layouts/main-layout/main-layout.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Sidebar } from '../../../shared/components/sidebar/sidebar';
 import { Header } from '../../../shared/components/header/header';
 import { RouterOutlet } from '@angular/router';
+import { NavigationService } from '../../services/navigation.service';
 
 @Component({
   selector: 'app-main-layout',
+  standalone: true,
   imports: [Sidebar, Header, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
 export class MainLayout {
-
+  private navigationService = inject(NavigationService);
 }
