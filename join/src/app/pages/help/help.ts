c66cd3929c13f71c3e72896ea1c50b557e878a8f
Dani S. <daniela.schomaker@posteo.de>
2026-01-12 10:31:26 +0100

    docs(help): add TSDoc comments to Help component

--- join/src/app/pages/help/help.ts
+++ join/src/app/pages/help/help.ts
@@ -2,6 +2,14 @@ import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavigationService } from '../../core/services/navigation.service';
 
+/**
+ * Help page component.
+ *
+ * @remarks
+ * Provides navigation logic for returning the user to the previously
+ * visited route. If no previous route is available, the user is redirected
+ * to the summary page.
+ */
 @Component({
   selector: 'app-help',
   standalone: true,
@@ -9,9 +17,24 @@ import { NavigationService } from '../../core/services/navigation.service';
   styleUrl: './help.scss',
 })
 export class Help {
+  /**
+   * Angular Router instance used for programmatic navigation.
+   */
   private router = inject(Router);
+
+  /**
+   * Service that stores and provides the previously visited route.
+   */
   private navigationService = inject(NavigationService);
 
+  /**
+   * Navigates the user back to the previous route.
+   *
+   * @remarks
+   * If a previous URL is available via the NavigationService,
+   * navigation will return to that route. Otherwise, the user
+   * is redirected to the summary page as a fallback.
+   */
   goBack(): void {
     const previousUrl = this.navigationService.getPreviousUrl();
 
@@ -23,4 +46,3 @@ export class Help {
     this.router.navigate(['/summary']);
   }
 }
-
